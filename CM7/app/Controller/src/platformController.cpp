d99a02c4e7c31a3b8a4a15c5568a4149b3afd8d4
mkaradeli <59120408+mkaradeli@users.noreply.github.com>
2026-09-17 01:19:02 +0300

    HWIL 4, v_td 3m/s, a_dec 3.7m/s2, v_max 5m/s yapilmistir.

--- CM7/app/Controller/src/platformController.cpp
+++ CM7/app/Controller/src/platformController.cpp
@@ -9,7 +9,7 @@
 //
 // Model version                  : 1.116
 // Simulink Coder version         : 25.2 (R2025b) 28-Jul-2025
-// C/C++ source code generated on : Thu Sep 17 00:58:50 2026
+// C/C++ source code generated on : Thu Sep 17 01:17:07 2026
 //
 // Target selection: ert.tlc
 // Embedded hardware selection: ARM Compatible->ARM Cortex-M
@@ -24,10 +24,10 @@
 
 // Exported block parameters
 struct_RzX5A87yYhLUhlAxm0ffzC platform_targets{
-  0.2,
+  0.3,
   0.1,
   1.0,
-  3.5,
+  3.7,
   5.0,
   5.0,
   0.02,
